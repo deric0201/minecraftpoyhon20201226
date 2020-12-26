f31a6a98ac4108872050fef9f3995bd00eb33bbd
deric0201 <74003143+deric0201@users.noreply.github.com>
2020-12-26 12:07:28 +0800

    Add files via upload

---
+++ 1226-3.py
@@ -0,0 +1,13 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+import time
+x,y,z = mc.player.getTilePos()
+
+
+number = 1
+for i in range(8):    
+    for j in range(number):
+        mc.spawnEntity(x,y,z,60)
+    number = number*10
+    mc.postToChat("生成了"+str(number)+"隻蠹魚!")
+    time.sleep(1)
